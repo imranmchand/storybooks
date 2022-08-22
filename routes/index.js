1797d2ac03adbcc0ad849dcbd50412fd30839f35
Imran Chand <imran.mc@hotmail.com>
2022-08-22 17:34:18 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+// @desc Login/Landing page
+// @route GET /
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+// @desc Darshboard page
+// @route GET /dashboard
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+
+module.exports = router
